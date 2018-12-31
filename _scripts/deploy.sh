8299ea54fe1e42d6053dd755b50dbf8e376e5ade
Li Xi <xl404@cam.ac.uk>
2018-12-31 21:41:22 +0000

    Fixed simple typo

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -11,6 +11,6 @@ sudo forever start --append --uid "sample" server.js
 # Update reverse proxy
 echo 
 echo '-------------'
-echo Updating reverse proxy
+echo 'Updating reverse proxy'
 sudo cp ./sample.com.conf /etc/nginx/conf.d/sample.com.conf
 sudo systemctl restart nginx
